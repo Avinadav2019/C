9db5b477437c675eeee0a4b581710826fb7ff2df
Avin Vincent <pkilasala@gmail.com>
2022-04-26 16:21:26 +0300

    Initial commit

---
+++ continueStatement.c
@@ -0,0 +1,11 @@
+#include<stdio.h>;
+main(){
+int i=0;
+do{
+if(i==3){
+i=i+1;
+continue;
+        }
+        printf("Hello World\n");
+    }while(i<5);
+}
---
+++ switchStatement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>;
+main(){
+    printf("Welcome to coding\n");
+    int x=2;
+    switch(x){
+        case 1:
+        printf("it is one");
+        break;
+        case 2:
+        printf("it is two");
+        break;
+        default:
+        printf("it is either 1 or 2");
+    }
+    }
